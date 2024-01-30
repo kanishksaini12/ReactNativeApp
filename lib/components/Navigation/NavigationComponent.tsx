76732b61fd97570c29cafe557bdacd96a981f655
kanishksaini12 <68396302+kanishksaini12@users.noreply.github.com>
2024-01-30 17:53:42 +0530

    navigation and sample static widget

---
+++ lib/components/Navigation/NavigationComponent.tsx
@@ -0,0 +1,38 @@
+import {BottomNavigation, Text} from 'react-native-paper';
+import {useState} from 'react';
+import TodoListComponent from '../TodoList/TodoListComponent';
+import WidgetComponent from '../Widgets/WidgetComponent';
+
+const HomeRoute = () => <Text>Home</Text>;
+const ToDoRoute = () => <TodoListComponent />;
+const WidgetsRoute = () => <WidgetComponent />;
+
+function NavigationComponent() {
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    {
+      key: 'home',
+      title: 'Home',
+      focusedIcon: 'heart',
+      unfocusedIcon: 'heart-outline',
+    },
+    {key: 'todo', title: 'To-Do List', focusedIcon: 'bell'},
+    {key: 'widgets', title: 'Widgets', focusedIcon: 'history'},
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    home: HomeRoute,
+    todo: ToDoRoute,
+    widgets: WidgetsRoute,
+  });
+
+  return (
+    <BottomNavigation
+      navigationState={{index, routes}}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
+  );
+}
+
+export default NavigationComponent;
